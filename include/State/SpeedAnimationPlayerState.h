2fac36ede6039cf89978c09f61ed9e1fd163b76c
Yochai <yochai.benita@gmail.com>
2024-06-24 16:37:57 +0300

    push

--- include/State/SpeedAnimationPlayerState.h
+++ include/State/SpeedAnimationPlayerState.h
@@ -11,7 +11,7 @@ public:
     virtual void enter(Player*) override;
     virtual void update(Player*, float) override;
     //virtual void draw(Player*, sf::RenderWindow&) override;
-    //virtual void jump(Player*) override;
+    virtual void jump(Player*) override;
     //virtual void walk(Player*) override {};
 
 private: //לבדוק אם להעלות ממברים למעלה
