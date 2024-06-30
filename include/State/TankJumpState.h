fe2fa150a2b0d7d976405c632b340d12b4f74c72
Benjamin Rosin <benjaminro@std.hac.ac.il>
2024-06-30 20:41:19 +0300

    jump state

--- include/State/TankJumpState.h
+++ include/State/TankJumpState.h
@@ -7,8 +7,6 @@ public:
     virtual void enter(Player*) override;
     virtual void update(Player*, float) override;
     virtual int getXOffset()const override{ return 85; };
-    //virtual void draw(Player*, sf::RenderWindow&) override;
-    //virtual void jump(Player*) override;
 
 private:
 
